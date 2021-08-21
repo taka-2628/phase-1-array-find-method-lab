2f557f2b532fc9600844784e23a46941e487a032
Takashi <th2826@columbia.edu>
2021-08-20 23:28:28 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -6,115 +6,60 @@ const record = [
 ]
 
 // Write a function that
-//      takes am array as an argument of objects 
+//      takes an array as an argument of objects 
 //          *note: record is an array that contains 3 objects
 //      uses find() method to test each object to see if the result is "W"
 //      return the year when the win occurred
 //      return undefined if no winis found 
 
-/*
-function superbowlWin(myArray) {
-    let yearWin = myArray.find(element => element.result === "W")
-      return yearWin.year
-  }
-  superbowlWin(record)
-*/ 
+// an already declared function as argument 
+function superbowlWin (array) {
+    function winFinder(element){
+        return element.result === "W"
+    }
+    const yearWin = array.find(winFinder)
+    if (yearWin){
+        return yearWin.year
+    } else {
+        return undefined
+    }
+}
 
 /*
-function superbowlWin(array) {
-    let yearWin = array.find(function(element){
-        if (element.result === "W"){
-            return array.year
-        } else {
-            return undefined
-        }
-    })
+// an anonymous function as argument 
+function superbowlWin (array) {
+    const yearWin = array.find(function(element) {return element.result === "W"})
+    if (yearWin) {
+        return yearWin.year;
+    } else {
+        return undefined
+    }
 }
 */
 
+/*
+// an arrow function as argument 
+function superbowlWin (array) {
+    const yearWin = array.find(element => element.result === "W")
+    if (yearWin) {
+        return yearWin.year;
+    } else {
+        return undefined
+    }
+}
+*/
+
+/* other ways to pass the test
 function superbowlWin(array) {
     let yearWin = array.find(function (element){
         if (element.result === "W"){
             return element.year
         } 
     })
-    return yearWin;
-}
-
-//superbowlWin(record)
-
-/*
-function superbowlWin(array) {
-    let yearWin = array.find(findWin)
-    function findWin(element){
-        if (element.result === "W"){
-            return element.year
-        } 
+    if (yearWin === undefined){
+        return undefined
+    } else{
+        return yearWin.year;
     }
-    return yearWin;
 }
-
-superbowlWin(record)
-*/
-
-/*
-function superbowlWin(array){
-    let found = array.find(function(object){
-        let isWin = (object.result === "w");
-        if (isWin = true) {
-            return isWin.year;
-        } else {
-            return undefined;
-        }
-    }) 
-    return found;
-}
-*/
-
-
-/*
-function superbowlWin(array){
-    let found = array.find(win) 
-    function win(object){
-        let isWin = object.result === "W";
-        if (isWin = "W"){
-            return object.year
-        } else {
-            return undefined
-        }
-    }
-    return found;
-}
-*/
-
-
-
-
-/*
-function superbowlWin(array){
-    function isWin(object){
-        if (object.result === "W"){
-            return object.year
-        } else {
-            return undefined
-        }
-    }
-    array.find(isWin);
-}
-
-console.log(superbowlWin(record));
-
-//console.log(superbowlWin(record));
-
-
-/*
-function isWin(item){
-    return item.result === 'W'
-}
-
-console.log(isWin(record))
-//console.log(record.find(isWin))
-
-
-//console.log(record.find(function(item){return item.result ==="W"}));
 */
